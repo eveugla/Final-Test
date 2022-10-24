86b0bc15ac399e6412ce1cea88c7bc2fb1b705d4
eveugla <pltevaz@mail.ru>
2022-10-24 21:34:51 +0300

    added Control Task

---
+++ Test/Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[11] {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+string[] array2 = new string[array1.Length];
+
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array2);
